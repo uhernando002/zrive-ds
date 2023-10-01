4e0f255f212a84e9119915556712c9653352fad1
uhernando002 <100877449+uhernando002@users.noreply.github.com>
2023-10-01 16:23:25 +0200

    new hypothesis: which type of items are the most ordered ones

--- src/module_2/module_2_groceries.py
+++ src/module_2/module_2_groceries.py
@@ -2,6 +2,7 @@ import pandas as pd
 import os
 import re
 import numpy as np
+from datetime import datetime
 
 
 DATA_PATH = "/home/unai/datasets/groceries/"
@@ -12,6 +13,7 @@ def main():
     a = extract_data(DATA_PATH)
     users_using_regulars(a[TABLES["regulars"]], a[TABLES["users"]])
     items_being_ordered(a[TABLES["inventory"]], a[TABLES["orders"]])
+    frequent_item_types(a[TABLES["inventory"]], a[TABLES["orders"]], 2022)
 
 
 def extract_data(data_path: str):
@@ -41,7 +43,6 @@ def users_using_regulars(
     number_of_users = len(users_info)
     percentage = regulars_info.user_id.nunique() * 100 / number_of_users
     print(f"The number of users that create regular items is of {round(percentage,2)}%")
-    # print(regulars_info.loc[regulars_info["variant_id"] == 33618848088195].empty)
 
 
 def items_being_ordered(
@@ -55,6 +56,25 @@ def items_being_ordered(
     percentage = len(ordered_items) * 100 / number_of_items
     print(f"The number of items that have been ordered is of {round(percentage,2)}%")
     print(len(np.intersect1d(ordered_items, inventory_info["variant_id"])))
+
+
+def frequent_item_types(
+    inventory_info: pd.DataFrame,
+    orders_info: pd.DataFrame,
+    year: int,
+):  # HIPOTESIS: Que tipo de productos son más comprados en cada año
+    idxs = [
+        idx for idx, order in orders_info.iterrows() if order["order_date"].year == year
+    ]
+    ordered_items = orders_info.iloc[idxs, :]
+    ordered_items = orders_info["ordered_items"].explode().reset_index(drop=True)
+    print(ordered_items)
+    ordered_items_types = pd.Series("NaN", index=np.arange(len(ordered_items)))
+    for k, ordered_item in enumerate(ordered_items):
+        item_info = inventory_info.loc[inventory_info["variant_id"] == ordered_item]
+        if not item_info.empty:
+            ordered_items_types[k] = item_info["product_type"].values[0]
+    print(ordered_items_types.value_counts().to_dict())
 
 
 if __name__ == "__main__":
