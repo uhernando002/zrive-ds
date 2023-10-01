395144eb983fb5541e90af502b6ea6a4e8f7605d
uhernando002 <100877449+uhernando002@users.noreply.github.com>
2023-10-01 12:14:28 +0200

    2 hypothesis: check how many users use regulars function and check how many items have been ordered once

--- src/module_2/module_2_groceries.py
+++ src/module_2/module_2_groceries.py
@@ -1,28 +1,30 @@
 import pandas as pd
 import os
 import re
+import numpy as np
 
 
 DATA_PATH = "/home/unai/datasets/groceries/"
+TABLES = {"abandoned_carts": 0, "users": 1, "regulars": 2, "orders": 3, "inventory": 4}
 
 
 def main():
     a = extract_data(DATA_PATH)
-    join_data(a)
+    users_using_regulars(a[TABLES["regulars"]], a[TABLES["users"]])
+    items_being_ordered(a[TABLES["inventory"]], a[TABLES["orders"]])
 
 
 def extract_data(data_path: str):
     data_list = []
     for file_name in os.listdir(data_path):
         data_list.append(
-            pd.read_parquet(os.path.join(data_path, file_name), engine="pyarrow")
-            .reset_index(drop=True)
-            .assign(table=re.sub(".parquet", "", file_name))
+            pd.read_parquet(
+                os.path.join(data_path, file_name), engine="pyarrow"
+            ).reset_index(drop=True)
         )
         print(file_name)
     print(data_list)
-    data = pd.concat(data_list, axis=0, sort=False)
-    return data
+    return data_list
     # print(data.columns.tolist())
     # print(data.head())
     # print(data.info())
@@ -32,10 +34,27 @@ def extract_data(data_path: str):
     # print(a.groupby(["user_id"])["variant_id"])
 
 
-def join_data(extracted_data: pd.DataFrame):
-    user_info = extracted_data[extracted_data["table"] == "orders"]
-    user_info = user_info.dropna(axis=1, how="all")
-    print(user_info.info())
+def users_using_regulars(
+    regulars_info: pd.DataFrame,
+    users_info: pd.DataFrame,
+):  # HIPOTESIS: Cuantos users usan la opción regulars
+    number_of_users = len(users_info)
+    percentage = regulars_info.user_id.nunique() * 100 / number_of_users
+    print(f"The number of users that create regular items is of {round(percentage,2)}%")
+    # print(regulars_info.loc[regulars_info["variant_id"] == 33618848088195].empty)
+
+
+def items_being_ordered(
+    inventory_info: pd.DataFrame,
+    orders_info: pd.DataFrame,
+):  # HIPOTESIS: Cuantos items han sido pedidos: RESULTADOS MUY RAROS !!!
+    number_of_items = len(inventory_info)
+    ordered_items = (
+        orders_info["ordered_items"].explode().reset_index(drop=True).unique()
+    )
+    percentage = len(ordered_items) * 100 / number_of_items
+    print(f"The number of items that have been ordered is of {round(percentage,2)}%")
+    print(len(np.intersect1d(ordered_items, inventory_info["variant_id"])))
 
 
 if __name__ == "__main__":
